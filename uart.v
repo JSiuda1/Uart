8bcec34400751f0655dc830376348477c458f417
JSiuda1 <jakkub68@gmail.com>
2024-03-24 12:29:16 +0100

    Data and end

--- uart.v
+++ uart.v
@@ -5,6 +5,9 @@ module uart(
 	output reg output_tx,
 	output reg [1:0] led
 );
+	// Temporary
+	parameter [7:0] data_byte = 65;  // A
+
 	// uart TX states
 	parameter TX_IDLE = 2'b00;
 	parameter TX_START = 2'b01;
@@ -21,6 +24,7 @@ module uart(
 	parameter clks_per_byte = clk_freq / baudrate;
 	reg [1:0] tx_state = TX_IDLE;
 	reg [7:0] clk_count = 0;
+	reg tx_bit_index = 0;
 	
 	// always on positive clock edge
 	always @(posedge clk)
@@ -30,17 +34,61 @@ module uart(
 			TX_IDLE:
 			begin
 				output_tx <= 1;	// Set tx line HIGH to indicate idle state
-				clk_count <= 0;
-				
 				if (data_ready == 0)  // When data ready change state to TX_START
-				begin
-					tx_state <= TX_START;
-				end
+					begin
+						clk_count <= 0;
+						tx_state <= TX_START;
+					end
 			end
 			
 			TX_START:
 			begin
 				output_tx <= 0;  // Set tx line LOW - start transmition
+				if (clk_count < clks_per_byte - 1)
+					begin
+					clk_count <= clk_count + 1;
+					end
+				else 
+					begin
+					clk_count <= 0;
+					tx_state <= TX_DATA;
+					end
+			end
+			
+			TX_DATA:
+			begin
+				output_tx <= data_byte[tx_bit_index];
+				if (clk_count < clks_per_byte - 1)
+					begin
+					clk_count <= clk_count + 1;
+					end
+				else
+					begin
+					clk_count <= 0;
+					if (tx_bit_index < 7)
+						begin
+						tx_bit_index <= tx_bit_index + 1;
+						end
+					else
+						begin
+						tx_bit_index <= 0;
+						tx_state <= TX_END;
+						end
+					end
+			end
+			
+			TX_END:
+			begin
+				output_tx <= 1;
+				if (clk_count < clks_per_byte - 1)
+					begin
+					clk_count <= clk_count + 1;
+					end
+				else 
+					begin
+					clk_count <= 0;
+					tx_state <= TX_IDLE;
+					end
 			end
 		endcase
 	end
